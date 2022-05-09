b14a09f7592cdc656e44d0f851b9273c629c8372
revolunet <revolunet@users.noreply.github.com>
2022-05-09 18:39:59 +0000

    Deploying to gh-pages from @ SocialGouv/matomoboard@cbe7ba4b98f7604c4adfdc4defad955103116a54 🚀

---
+++ _next/static/AiUJj-M3gkxHhtEXQlz1Q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/602-96dd267d7ad9a1fd.js","static/chunks/pages/index-a7d29e0ffd80651f.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
